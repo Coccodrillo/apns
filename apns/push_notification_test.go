cad8ecf45877fc7e14b821b9c458e47ed7b6ae88
Alan Harris <ah.anachronistic@gmail.com>
2013-05-19 13:10:00 -0400

    Cleaning up the tests a bit.

--- apns/push_notification_test.go
+++ apns/push_notification_test.go
@@ -4,77 +4,85 @@ import (
 	"testing"
 )
 
-func TestBasicAlert(t *testing.T) {
-	message := new(Message)
-	message.Alert = "You have mail!"
-
-	envelope := new(Envelope)
-	envelope.SetMessage(message)
-
-	bytes, _ := envelope.ToBytes()
-	if len(bytes) != 47 {
-		t.Error("expected 47 bytes; got", len(bytes))
-	}
-
-	json, _ := envelope.PayloadJSON()
-	if len(json) != 34 {
-		t.Error("expected 34 bytes; got", len(json))
-	}
+// Create a new Payload that specifies simple text,
+// a badge counter, and a custom notification sound.
+func mockPayload() (payload *Payload) {
+	payload = new(Payload)
+	payload.Alert = "You have mail!"
+	payload.Badge = 42
+	payload.Sound = "bingbong.aiff"
+	return
 }
 
-func TestDictionaryAlert(t *testing.T) {
+// Create a new DictionaryAlert. Apple recommends you not use
+// the more complex alert style unless absolutely necessary.
+func mockDictionaryAlert() (dict *DictionaryAlert) {
 	args := make([]string, 1)
 	args[0] = "localized args"
 
-	dict := new(DictionaryAlert)
+	dict = new(DictionaryAlert)
 	dict.Body = "Complex Message"
 	dict.ActionLocKey = "Play a Game!"
 	dict.LocKey = "localized key"
 	dict.LocArgs = args
 	dict.LaunchImage = "image.jpg"
+	return
+}
 
-	message := new(Message)
-	message.Badge = 42
-	message.Sound = "bingbong.aiff"
-	message.Alert = dict
-
+func TestBasicAlert(t *testing.T) {
+	payload := mockPayload()
 	envelope := new(Envelope)
-	envelope.SetMessage(message)
+
+	envelope.AddPayload(payload)
 
 	bytes, _ := envelope.ToBytes()
+	json, _ := envelope.PayloadJSON()
+	if len(bytes) != 82 {
+		t.Error("expected 82 bytes; got", len(bytes))
+	}
+	if len(json) != 69 {
+		t.Error("expected 69 bytes; got", len(json))
+	}
+}
+
+func TestDictionaryAlert(t *testing.T) {
+	dict := mockDictionaryAlert()
+	payload := mockPayload()
+	payload.Alert = dict
+
+	envelope := new(Envelope)
+	envelope.AddPayload(payload)
+
+	bytes, _ := envelope.ToBytes()
+	json, _ := envelope.PayloadJSON()
 	if len(bytes) != 207 {
 		t.Error("expected 207 bytes; got", len(bytes))
 	}
-
-	json, _ := envelope.PayloadJSON()
 	if len(json) != 194 {
 		t.Error("expected 194 bytes; got", len(bytes))
 	}
 }
 
 func TestCustomParameters(t *testing.T) {
-	message := new(Message)
-	message.Alert = "You have mail!"
-
+	payload := mockPayload()
 	envelope := new(Envelope)
-	envelope.SetMessage(message)
-	envelope.Set("Doctor", "Who")
 
-	bytes, _ := envelope.ToBytes()
-	if len(bytes) != 62 {
-		t.Error("expected 62 bytes; got", len(bytes))
-	}
+	envelope.AddPayload(payload)
+	envelope.Set("foo", "bar")
 
-	json, _ := envelope.PayloadJSON()
-	if len(json) != 49 {
-		t.Error("expected 49 bytes; got", len(bytes))
-	}
-
-	if envelope.Get("Doctor") != "Who" {
+	if envelope.Get("foo") != "bar" {
 		t.Error("unable to set a custom property")
 	}
-
-	if envelope.Get("Not Set") != nil {
+	if envelope.Get("not_set") != nil {
 		t.Error("expected a missing key to return nil")
+	}
+
+	bytes, _ := envelope.ToBytes()
+	json, _ := envelope.PayloadJSON()
+	if len(bytes) != 94 {
+		t.Error("expected 94 bytes; got", len(bytes))
+	}
+	if len(json) != 81 {
+		t.Error("expected 81 bytes; got", len(json))
 	}
 }
