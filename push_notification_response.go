e5499e7425391633fd856a4dd887e84f9b187493
Alan Harris <ah.anachronistic@gmail.com>
2013-05-29 16:07:20 -0400

    Adding a push notification response struct.

---
+++ push_notification_response.go
@@ -0,0 +1,35 @@
+package apns
+
+import ()
+
+// The maximum number of seconds we're willing to wait for a response
+// from the Apple Push Notification Service.
+const TIMEOUT_SECONDS = 3
+
+// This enumerates the response codes that Apple defines
+// for push notification attempts.
+var APPLE_PUSH_RESPONSES = map[uint8]string{
+	0:   "NO_ERRORS",
+	1:   "PROCESSING_ERROR",
+	2:   "MISSING_DEVICE_TOKEN",
+	3:   "MISSING_TOPIC",
+	4:   "MISSING_PAYLOAD",
+	5:   "INVALID_TOKEN_SIZE",
+	6:   "INVALID_TOPIC_SIZE",
+	7:   "INVALID_PAYLOAD_SIZE",
+	8:   "INVALID_TOKEN",
+	10:  "SHUTDOWN",
+	255: "UNKNOWN",
+}
+
+type PushNotificationResponse struct {
+	Success       bool
+	AppleResponse string
+	Error         error
+}
+
+func NewPushNotificationResponse() (resp *PushNotificationResponse) {
+	resp = new(PushNotificationResponse)
+	resp.Success = false
+	return
+}
