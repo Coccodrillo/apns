33bea02c8f760e39f3ff18bfe763b450b96b32a1
Victor Blomqvist <blomqvist@p1.cn>
2014-01-06 15:18:18 +0800

    doubled receive error timeout

--- push_notification_response.go
+++ push_notification_response.go
@@ -4,7 +4,7 @@ import ()
 
 // The maximum number of seconds we're willing to wait for a response
 // from the Apple Push Notification Service.
-const TIMEOUT_SECONDS = 5
+const TIMEOUT_SECONDS = 10
 
 // This enumerates the response codes that Apple defines
 // for push notification attempts.
